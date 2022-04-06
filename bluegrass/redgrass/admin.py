d672782ee3c01f342e9147e24454dc1c27374528
PARENT <parent@HonYuLUNG-7N1.local>
2022-04-06 17:37:58 +0800

    first commit

---
+++ bluegrass/redgrass/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from django.contrib.admin.models import LogEntry
+
+admin.site.register(LogEntry)
---
+++ bluegrass/redgrass/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RedgrassConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'redgrass'
